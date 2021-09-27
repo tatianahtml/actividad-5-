657d8a853974f9bec5ab752dd4feffb114dc64a5
tatianahtml <yicecarval@gmail.com>
2021-09-26 22:45:23 -0500

    proyecto5

---
+++ js/Script.js
@@ -0,0 +1,58 @@
+  // alert("Bienvenidos a esta pagina");
+  //prompt("ingresa el nombre de la seccion a investigar");
+  //confirm("deseas salir de la pagina");
+
+
+  function nombreSitioWeb() {
+    var nombreSitio = prompt ("como se llama este sitio web");
+
+    if (nombreSitio == " Globalización ") {
+        alert ("El nombre es correcto");
+    } else {
+        alert ("el nombre es incorrecto");
+    }
+  }
+
+
+  function confirmacion () {
+       return confirm("Quieres ir a esta red social?")
+  }
+
+  function confirmacion () {
+    return confirm("Quieres ir a esta red social?")
+}
+
+function confirmacion () {
+    return confirm("Quieres ir a esta red social?")
+}
+
+ $(document).ready(function () {
+   $("#parrafo").css("font-size","30px");
+ }
+
+ );
+
+ $("parrafo2").css("background" ,"black").css ("color","white");
+
+ function nombreSitioWeb() {
+  var nombreSitio = prompt ("como se llama este sitio web");
+
+  if (nombreSitio == " Globalización ") {
+      alert ("El nombre es correcto");
+  } else {
+      alert ("el nombre es incorrecto");
+  }
+}
+
+
+function confirmacion () {
+     return confirm("Quieres ir a esta red social?")
+}
+
+function confirmacion () {
+  return confirm("Quieres ir a esta red social?")
+}
+
+function confirmacion () {
+  return confirm("Quieres ir a esta red social?")
+}
